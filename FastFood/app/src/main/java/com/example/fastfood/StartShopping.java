55904fc5c5ba603ced89cc5405695df7cde2734b
DanielI97 <adindaniel1997@hotmail.com>
2020-07-02 22:48:48 -0500

    Change color menu star shopping
    cambio colores y anadir segementos en spinner

--- FastFood/app/src/main/java/com/example/fastfood/StartShopping.java
+++ FastFood/app/src/main/java/com/example/fastfood/StartShopping.java
@@ -8,26 +8,34 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
+
 public class StartShopping extends AppCompatActivity {
 
     private Spinner spinner;
+    private Spinner spinner2;
+    private Spinner spinner3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_shopping);
         spinner=(Spinner)findViewById(R.id.spinner1);
-
-        String []opciones=new String[]{"Quito","Sangolqui"};
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opciones);
+        spinner2= (Spinner)findViewById(R.id.spinner2);
+        spinner3= (Spinner)findViewById(R.id.spinner3);
+        String []pais=new String[]{"Ecuador"};
+        String []ciudad=new String[]{"Sangolqui"};
+        String []sector=new String[]{"Selva Alegre", "Salcoto", "Cashapamba", "Espe"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,pais);
         spinner.setAdapter(adapter);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,ciudad);
+        spinner2.setAdapter(adapter1);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,sector);
+        spinner3.setAdapter(adapter2);
     }
 
     public void MainMenushopping(View view){
         Intent MainMenuShoopers = new Intent(this,MainMenuShoopers.class);
         startActivity(MainMenuShoopers);
     }
-    /*List<String> listSpinner = Arrays.asList("item1", "item2", "item3");
-ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            android.R.layout.simple_spinner_item, listSpinner);*/
 }
